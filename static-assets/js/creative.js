d6d7aee626df25b97d88523cd14336e181688a31
Jihyeong Lee <leejh1998@hotmail.com>
2016-08-13 18:25:06 -0400

    Made homepage using Creative Bootstrap template

--- static-assets/js/creative.js
+++ static-assets/js/creative.js
@@ -52,7 +52,7 @@
         tLoading: 'Loading image #%curr%...',
         mainClass: 'mfp-img-mobile',
         gallery: {
-            enabled: true,
+            enabled: false,
             navigateByImgClick: true,
             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
